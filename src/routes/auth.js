a6823b6a81c251fe5ab70295edcc1f7cc91068f9
zameer <zameeralucknowi@gmail.com>
2025-06-06 04:20:56 +0530

    cookie added properly

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -40,7 +40,13 @@ router.post('/login', async(req,res)=>{
         if(!isValidPassword)
         throw new Error('Invalid credentials');
         const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'3d'});
-        res.cookie('token',token,{expires:new Date(Date.now() + 24 * 3600000)});
+        res.cookie('token',token,
+            {expires:new Date(Date.now() + 24 * 3600000),
+                  httpOnly: true,
+                  sameSite: 'None',    // IMPORTANT for cross-site requests
+                  secure: true         // IMPORTANT for HTTPS-only (Render uses HTTPS)
+            }         
+        );
         res.status(200).json({message:'login successfully', data:user})
     } catch (error) {
         res.status(400).send(error.message)
